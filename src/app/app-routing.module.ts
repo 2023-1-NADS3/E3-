258923a1725100b3bc904241bf037410070e8733
PedroRodante <pedrorodantevicente@gmail.com>
2023-03-16 17:28:18 -0300

    Comitando o primeiro passo.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+
+export class CadastroComponent {
+
+  criarConta() {
+    if (this.formValido()) {
+      // Enviar os dados para o backend
+    }
+  }
+
+  formValido(): boolean {
+    return true;
+  }
+}
