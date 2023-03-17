af8ebf2ed01c8c980eb1246e3e083ded5c0f2c11
PedroRodante <pedrorodantevicente@gmail.com>
2023-03-17 10:15:56 -0300

    Testes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,4 +18,5 @@ import { CadastroComponent } from './cadastro/cadastro.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -18,3 +18,4 @@ export class CadastroComponent {
     return true;
   }
 }
+export class AppModule { }
