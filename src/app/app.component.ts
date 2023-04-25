a0da8544f6441451b4a257ad44c3258f49b586cf
PedroRodante <pedrorodantevicente@gmail.com>
2023-04-24 22:39:36 -0300

    Aulinha do Victor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery' ;
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,23 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'SlowFu';
+
+  Conectar(){
+    $.get("http://localhost:3000/",
+      function(resultado)
+      {
+        console.log(resultado);
+      });
+  }
+
+  EnviarDados(valor:string)
+  {
+    var json = {"dado": valor};
+    $.post("http://localhost:3000/dado",
+    json,
+    function (msg)
+    {
+        console.log(msg);
+    });
+  }
 }
