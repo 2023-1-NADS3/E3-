b12c3220839da79eee985e54aa456a338002e35e
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-10 05:21:03 -0300

    Muita Choradeira/ Pouco avanço

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import * as $ from 'jquery' ;
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,23 +11,33 @@ import * as $ from 'jquery' ;
 
 export class CadastroComponent {
 
+  constructor(private http: HttpClient, private router: Router) { }
+  emailJaExiste = false;
+
   CriarConta(nome:string, senha:string, email:string, telefone:string)
   {
-    var json = {
-      "nome":nome, 
-      "senha":senha, 
-      "email":email, 
-      "telefone":telefone
-    };
-
-    $.post("http://localhost:3000/cadastro_usuario",
-    json,
-    function (msg)
-    {
-        console.log(msg);
+      console.log("Passei no primeiro ponto do cadastro");
+      var json = {
+        "nome":nome, 
+        "senha":senha, 
+        "email":email, 
+        "telefone":telefone
+      };
+      
+      console.log("Passei no segundo ponto do cadastro");
+      $.post("http://localhost:3000/cadastro_usuario", json, (res) => // Usa arrow function
+      {
+        console.log("Passei no terceiro ponto do cadastro");
+        console.log(res);
+        if (res === "Email já existe") {
+          this.emailJaExiste = true;
+        } else {
+          console.log("Usuário adicionado!");
+          this.router.navigate(['/seletor']);// Usa a referência ao objeto atual
+        }
     });
   }
-
+  
   formValido(): boolean {
     return true;
   }
