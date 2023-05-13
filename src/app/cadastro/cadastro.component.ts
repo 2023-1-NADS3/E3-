dfe5be6868a24c0807b4084102fcd38a4cc88201
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-13 09:22:46 -0300

    Testando cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -20,32 +20,33 @@ export class CadastroComponent {
     if(termo.checked){
     if(nome.length < 3  || nome.length > 30){
       alert("Seu nome precisa ter entre 3 e 30 caracteres.");
-      window.location.href = "/cadastro";
+      //window.location.href = "/cadastro";
       console.log("Seu nome precisa ter entre 3 e 30 caracteres.");
     }
     else if(senha.length < 6 || senha.length > 12){
       alert("Sua senha precisa ter entre 6 e 12 caracteres.");
-      window.location.href = "/cadastro";
+      //window.location.href = "/cadastro";
       console.log("Sua senha precisa ter entre 6 e 12 caracteres.");
     }
     else if(telefone.length < 11 || telefone.length > 11){
       alert("Escreva um telefone válido com DDD. EX:11 98765-4321");
-      window.location.href = "/cadastro";
+      //window.location.href = "/cadastro";
       console.log("Escreva um telefone válido com DDD. EX:11 98765-4321");
     }
     else{
-    $.post("http://localhost:3000/cadastro_usuario", {
+      console.log("Passei no segundo ponto do cadastro");
+      $.post("http://localhost:3000/cadastro_usuario", {
       "nome":nome, 
       "senha":senha, 
       "email":email, 
       "telefone":telefone
     }, 
     (res) => {
-      console.log("Passei no segundo ponto do cadastro");
+      console.log("Passei no terceiro ponto do cadastro");
       console.log(res);
       if (res === "Email já existe") {
         alert("Esse email já foi cadastrado.");
-        window.location.href = "/cadastro";
+        //window.location.href = "/cadastro";
       }
       else {
         console.log("Usuário adicionado!");
@@ -56,9 +57,9 @@ export class CadastroComponent {
     }
     else{
       alert("Você precisa concordar com os Termos de Privacidade.");
-      window.location.href = "/";
       console.log("Você precisa concordar com os Termos de Privacidade.");
-      window.location.href = "/cadastro";
+      //window.location.replace("/cadastro");
+      //window.location.href = "/cadastro";
     }
   }
   
