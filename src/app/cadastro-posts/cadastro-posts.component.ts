7e820272ba2464c8e21e0a7b384b24d8f6ff3e8e
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-22 18:28:07 -0300

    Começando a função dos posts

--- src/app/cadastro-posts/cadastro-posts.component.ts
+++ src/app/cadastro-posts/cadastro-posts.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery' ;
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro-posts',
@@ -7,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class CadastroPostsComponent {
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+  CriarPost(titulo:string, valor:string, descricao:string) 
+  {
+    console.log("Passei no primeiro ponto do cadastro");
+    let termo = document.getElementById("termo") as HTMLInputElement;
+
+    if(titulo.length < 3  || titulo.length > 20){
+      alert("Seu titulo precisa ter entre 3 e 30 caracteres.");
+      console.log("Seu titulo precisa ter entre 3 e 30 caracteres.");
+    }
+    else if(descricao.length == 0){
+      alert("Você precisa de uma descrição =)");
+      console.log("O campo descrição está vazio");
+    }
+    else{
+      console.log("Passei no segundo ponto do cadastro");
+      $.post("http://localhost:3000/cadastro_usuario", {
+      "titulo":titulo, 
+      "valor":valor,
+      "descricao":descricao, 
+      //"data":data,
+      //"local":local,
+      //emailSalvo:email, 
+      //telefoneSalvo:telefone
+    }, 
+    (res) => {
+      console.log("Passei no terceiro ponto do cadastro");
+      console.log(res);
+      window.location.href = "/cadastro-posts";
+    });
+    }
+  }
+
+  SeusPost()
+  {
+      
+  }
 }
