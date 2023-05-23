87658c7b1d51cebb32db7dd52de922cc1a5b9e0e
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-23 07:56:17 -0300

    Avanço no cadastro e na Visualização dos posts

--- src/app/cadastro-posts/cadastro-posts.component.ts
+++ src/app/cadastro-posts/cadastro-posts.component.ts
@@ -1,34 +1,34 @@
 import { Component } from '@angular/core';
-import * as $ from 'jquery' ;
+import * as $ from 'jquery';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { TermoPrivacidadeComponent } from '../termo-privacidade/termo-privacidade.component';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-cadastro-posts',
   templateUrl: './cadastro-posts.component.html',
-  styleUrls: ['./cadastro-posts.component.css']
+  styleUrls: ['./cadastro-posts.component.css'],
 })
 export class CadastroPostsComponent {
-
-  constructor(private http: HttpClient, private router: Router) 
-  { 
-    this.dataAtual = new Date(); 
+  constructor(private http: HttpClient, private router: Router) {
+    this.dataAtual = new Date();
     this.localizacao = {};
+    this.posts = [];
   }
 
   dataAtual: Date;
   localizacao: any;
+  posts: any[];
 
-  obterLocalizacao() {
-    
+  ngOnInit(){
+    this.SeusPosts();
   }
 
-  //CriarPost(titulo:string, valor:string, tipo:string, descricao:string, data:string, local:string, email:string, telefone:string) 
-  CriarPost(titulo:string, valor:string, tipo:string, descricao:string) 
-  {
-    console.log("Passei no primeiro ponto do cadastro");
+  exibirPosts(posts: any[]) {
+    this.posts = posts;
+  }
 
+  obterLocalizacao() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (posicao) => {
@@ -44,35 +44,55 @@ export class CadastroPostsComponent {
     } else {
       console.log('Geolocalização não suportada pelo navegador.');
     }
+  }
 
-    if(titulo.length < 3  || titulo.length > 20){
-      alert("Seu titulo precisa ter entre 3 e 30 caracteres.");
-      console.log("Seu titulo precisa ter entre 3 e 30 caracteres.");
-    }
-    else if(descricao.length == 0){
-      alert("Você precisa de uma descrição =)");
-      console.log("O campo descrição está vazio");
-    }
-    else{
-      console.log("Passei no segundo ponto do cadastro");
-      $.post("http://localhost:3000/cadastro_usuario", {
-      "titulo":titulo, 
-      "valor":valor,
-      "descricao":descricao, 
-      "tipo":tipo,
-      "data":this.dataAtual,
-      "local":this.localizacao,
-    }, 
-    (res) => {
-      console.log("Passei no terceiro ponto do cadastro");
-      console.log(res);
-      window.location.href = "/cadastro-posts";
-    });
+  //CriarPost(titulo:string, valor:string, tipo:string, descricao:string, data:string, local:string, email:string, telefone:string)
+  CriarPost(titulo: string, valor: string, tipo: string, descricao: string) {
+    console.log('Passei no primeiro ponto do cadastro');
+    let dataFormatada = format(this.dataAtual, 'dd.MM.yy');
+
+    if (titulo.length < 3 || titulo.length > 20) {
+      alert('Seu titulo precisa ter entre 3 e 30 caracteres.');
+      console.log('Seu titulo precisa ter entre 3 e 30 caracteres.');
+    } else if (descricao.length == 0) {
+      alert('Você precisa de uma descrição =)');
+      console.log('O campo descrição está vazio');
+    } else {
+      console.log('Passei no segundo ponto do cadastro');
+      this.obterLocalizacao();
+      $.post(
+        'http://localhost:3000/cadastro_post',
+        {
+          "titulo": titulo,
+          "valor": valor,
+          "descricao": descricao,
+          "tipo": tipo,
+          "data": dataFormatada,
+          "local": this.localizacao,
+        },
+        (res) => {
+          console.log('Passei no terceiro ponto do cadastro');
+          console.log(res);
+          window.location.href = '/cadastro-posts';
+        }
+      );
     }
   }
 
-  SeusPost()
-  {
-      
+  SeusPosts() {
+    $.get('http://localhost:3000/posts_usuario', {}, (res) => {
+      let posts = res;
+      this.exibirPosts(posts);
+    });
+  }
+  
+  DeletarPost(postID:string){
+    $.post('http://localhost:3000/delete_post_usuario', {
+      "postID":postID
+    }, (res) => {
+      console.log(res);
+      window.location.href = '/cadastro-posts';
+      this.SeusPosts();
+    });
   }
 }
--- src/app/seletor/seletor.component.ts
+++ src/app/seletor/seletor.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-seletor',
@@ -7,5 +10,22 @@ import { Component } from '@angular/core';
 })
 
 export class SeletorComponent {
+  constructor(private http: HttpClient, private router: Router) {this.posts = [];}
 
+  posts: any[];
+
+  ngOnInit(){
+    this.Posts();
+  }
+
+  exibirPosts(posts: any[]) {
+    this.posts = posts;
+  }
+
+  Posts() {
+    $.get('http://localhost:3000/todos_posts', {}, (res) => {
+      let posts = res;
+      this.exibirPosts(posts);
+    });
+  }
 }
