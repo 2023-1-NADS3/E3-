4166e0379785dd3d44a1a542b79f26e90726c5e4
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-26 07:39:52 -0300

    Alteração de Perfil e Delete funcionando! É SÓ COISA BOA MEU POVO

--- src/app/perfil-edicao/perfil-edicao.component.ts
+++ src/app/perfil-edicao/perfil-edicao.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery' ;
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-perfil-edicao',
   templateUrl: './perfil-edicao.component.html',
-  styleUrls: ['./perfil-edicao.component.css']
+  styleUrls: ['./perfil-edicao.component.css'],
 })
 export class PerfilEdicaoComponent {
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  AlterarDados(
+    nome: string,
+    senhaAtual: string,
+    senha: string,
+    email: string,
+    telefone: string
+  ) {
+    console.log('Passei no primeiro ponto da altereção');
+
+    $.get(
+      'http://localhost:3000/dados_usuario',{},
+      (res) => {
+        console.log('Passei no check da senha.');
+        console.log(res);
+        console.log(res[0].senha);
+        
+        if(senhaAtual != res[0].senha){
+          console.log("A senha atual foi digitada errada.");
+          alert('Parece que você digitou sua senha atual errado. Tente de novo.');
+        }
+        else{    
+          if (nome.length < 3 || nome.length > 30) {
+          alert('Seu nome precisa ter entre 3 e 30 caracteres.');
+          console.log('Seu nome precisa ter entre 3 e 30 caracteres.');
+        } else if (senha.length < 6 || senha.length > 12) {
+          alert('Sua senha precisa ter entre 6 e 12 caracteres.');
+          console.log('Sua senha precisa ter entre 6 e 12 caracteres.');
+        } else if (telefone.length < 11 || telefone.length > 11) {
+          alert('Escreva um telefone válido com DDD. EX:11 98765-4321');
+          console.log('Escreva um telefone válido com DDD. EX:11 98765-4321');
+        } else {
+          console.log('Passei no segundo ponto da alteração');
+          $.post(
+            'http://localhost:3000/alterar_dados_usuario',
+            {
+              nome: nome,
+              senha: senha,
+              email: email,
+              telefone: telefone,
+            },
+            (res) => {
+              console.log('Passei no terceiro ponto da alteração');
+              console.log(res);
+              console.log('Dados alterados!');
+              window.location.href = '/perfil-proprio-usuario';
+            }
+          );
+        }}
+      }
+    );
+  }
 }
--- src/app/perfil-proprio-usuario/perfil-proprio-usuario.component.ts
+++ src/app/perfil-proprio-usuario/perfil-proprio-usuario.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import * as $ from 'jquery' ;
 
 @Component({
   selector: 'app-perfil-proprio-usuario',
   templateUrl: './perfil-proprio-usuario.component.html',
-  styleUrls: ['./perfil-proprio-usuario.component.css']
+  styleUrls: ['./perfil-proprio-usuario.component.css'],
 })
-export class PerfilProprioUsuarioComponent {
+export class PerfilProprioUsuarioComponent implements OnInit {
+  nome: string = '';
+  email: string = '';
+  telefone: number = 0;
+  senha: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {
+    this.Dados();
+  }
+
+  Dados() {
+    console.log('Fui acionado');
+    this.http.get<any>('http://localhost:3000/dados_usuario').subscribe(
+      (res) => {
+        this.nome = res[0].nome; // Verifique a estrutura do objeto JSON retornado e ajuste os índices ou propriedades adequadamente
+        this.senha = res[0].senha;
+        this.email = res[0].email;
+        this.telefone = parseInt(res[0].telefone);
+
+        console.log('Trouxe dados!');
+        console.log(res);
+      },
+      (error) => {
+        console.log('Ocorreu um erro ao obter os dados do usuário:', error);
+      }
+    );
+  }
+
+  Sair() {
+    console.log('Saindo!');
+    this.http.get<any>('http://localhost:3000/sair').subscribe(
+      (res) => 
+      {
+        console.log(res);
+        window.location.href = '/';
+      },
+      (error) => {
+        console.log('Ocorreu um erro ao obter os dados do usuário:', error);
+      }
+    );
+  }
+
+  DeletarUsuario(){
+    console.log("Função acionada");
+
+    $.post('http://localhost:3000/delete_usuario', {}, (res) => {
+      console.log(res);
+      window.location.href = '/';
+    });
+  }
 }
