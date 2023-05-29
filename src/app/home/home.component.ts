b64bb85d93218d5b14bf1b7c71a97e9720ed9a1c
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-28 22:20:04 -0300

    Tinder rolando

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  // código do componente aqui
+export class HomeComponent implements OnInit{
+  showPreloader: boolean = true;
+  hideContainer: boolean = false;
+
+  constructor(){}
+
+  ngOnInit()
+  {
+    setTimeout(() => {
+      this.showPreloader = false;
+      this.hideContainer = true;
+    }, 3000);
+  }
+  
 }
--- src/app/seletor/seletor.component.ts
+++ src/app/seletor/seletor.component.ts
@@ -1,38 +1,40 @@
 import { Component } from '@angular/core';
-import * as $ from 'jquery';
-import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { get } from 'jquery';
 
 @Component({
   selector: 'app-seletor',
   templateUrl: './seletor.component.html',
   styleUrls: ['./seletor.component.css']
 })
-
 export class SeletorComponent {
-  constructor(private http: HttpClient, private router: Router) {this.posts = [];}
+  posts: any[] = [];
+  currentPostIndex: number = 0;
+  numero: string = '';
 
-  posts: any[];
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(){
-    this.Posts();
+  ngOnInit() {
+    this.getPosts();
   }
 
-  exibirPosts(posts: any[]) {
-    this.posts = posts;
-  }
-
-  Posts() {
-    $.get('http://localhost:3000/todos_posts', {}, (res) => {
-      let posts = res;
-      this.exibirPosts(posts);
+  getPosts() {
+    this.http.get('http://localhost:3000/todos_posts').subscribe((res: any) => {
+      this.posts = res;
+      this.currentPostIndex = this.posts.length - 1;
     });
   }
 
-  Ligar(numero:string, nome:string)
-  {
+  proximoPost() {
+    this.currentPostIndex--;
+    if(this.currentPostIndex < 0)
+    {
+      this.currentPostIndex = this.posts.length - 1
+    }
+  }
+
+  Ligar(numero: string) {
     console.log(numero);
-    console.log(nome);
-    window.location.href = "https://api.whatsapp.com/send?phone="+{numero}+"&text=Esse%20é%20o%20whatsapp%20do%20seu%20novo%20fornecedor!%20Seja%20gentil%20e%20lembre%20de%20contar%20referente%20a%20que%20produto%20você%20está%20entrando%20em%20contato.";
+    window.location.href = `https://api.whatsapp.com/send?phone=${numero}&text=Esse%20é%20o%20whatsapp%20do%20seu%20novo%20fornecedor!%20Seja%20gentil%20e%20lembre%20de%20contar%20referente%20a%20que%20produto%20você%20está%20entrando%20em%20contato.`;
   }
 }
